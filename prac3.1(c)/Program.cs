ca03424c500c013cfc2f623cbd90fa6c0b2481c1
Susa1ev <susalytt@gmail.com>
2021-11-30 21:59:57 +0300

    Добавьте файлы проекта.

---
+++ prac3.1(c)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace prac3._1_c_
+{
+    class prac
+    {
+        public prac()
+        {
+            In();
+            Console.WriteLine(Main());
+        }
+        double ab, ac, cd;
+
+        double Main()
+        {
+            return ab + ac + cd + f(f(ab, ac), cd);
+        }
+        void In()
+        {
+            reTry:
+            Console.WriteLine("Введите AB,AC,CD:");
+            if (Double.TryParse(Console.ReadLine(), out ab) &&
+                Double.TryParse(Console.ReadLine(), out ac) &&
+                Double.TryParse(Console.ReadLine(), out cd))
+            { }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то, попробуйте ещё раз");
+                goto reTry;
+            }
+
+        }
+        double f(double a,double b)
+        {
+            double c = Math.Sqrt(a * a + b * b);
+            return c;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
